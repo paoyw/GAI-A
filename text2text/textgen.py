b5c611a5f1d836c697158e9c6f7300cb034e46cd
paoyw <vincent01pao@gmail.com>
2024-06-26 16:04:53 +0800

    Create the basic API endpoint using flask and llama3-8B

---
+++ text2text/textgen.py
@@ -0,0 +1,47 @@
+import json
+
+from flask import Flask, request
+import transformers
+import torch
+
+# Prepares the LLM model.
+model_id = "meta-llama/Meta-Llama-3-8B-Instruct"
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model_id,
+    model_kwargs={"torch_dtype": torch.bfloat16},
+    device_map="auto",
+)
+
+terminators = [
+    pipeline.tokenizer.eos_token_id,
+    pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+]
+
+print("The LLM model is prepared.")
+
+# The backend endpoints.
+app = Flask(__name__)
+
+@app.route("/textgen", methods=["POST"])
+def textgen():
+    request_data = request.get_json()
+    messages = [
+        {"role": "system",
+        "content": "You are a pirate chatbot who always responds in pirate speak!"},
+        {"role": "user",
+        "content": request_data["content"]},
+    ]
+    response_data = pipeline(
+        messages,
+        max_new_tokens=256,
+        eos_token_id=terminators,
+        do_sample=True,
+        temperature=0.6,
+        top_p=0.9
+    )
+    print(response_data)
+    return json.dumps(response_data)
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True, use_reloader=False)
